acf5b86460741e78bc447cadbcf96732c428bb38
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2023-12-03 19:08:10 +0100

    Add template day

---
+++ src/DayXX.kt
@@ -0,0 +1,23 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // Test input solution
+    val testInput = readInput("DayXX_test")
+    print("Expected Test for Part 1: ")
+    part1(testInput).println()
+    print("Expected Test for Part 2: ")
+    part2(testInput).println()
+
+    // Real input solution
+    val input = readInput("DayXX")
+    print("Part 1: ")
+    part1(input).println()
+    print("Part 2: ")
+    part2(input).println()
+}
