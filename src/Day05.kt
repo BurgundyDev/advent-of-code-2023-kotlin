ee1b41bd1d45d637aa762650b414860a3607e1bb
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2023-12-05 10:22:18 +0000

    Start Day 5

---
+++ src/Day05.kt
@@ -0,0 +1,23 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // Test input solution
+    val testInput = readInput("Day05_test")
+    print("Expected Test for Part 1: ")
+    part1(testInput).println()
+    print("Expected Test for Part 2: ")
+    part2(testInput).println()
+
+    // Real input solution
+    val input = readInput("Day05")
+    print("Part 1: ")
+    part1(input).println()
+    print("Part 2: ")
+    part2(input).println()
+}
