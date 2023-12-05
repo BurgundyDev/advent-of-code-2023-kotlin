e46695e9620d28dc5c624cbc287d9f78f2d8a7c8
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2023-12-05 23:15:42 +0100

    Add automated gradle task registering

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,14 @@ tasks {
         gradleVersion = "8.5"
     }
 }
+
+for(file in fileTree("src")) {
+    if(file.name.matches(Regex("Day\\d{2}.kt"))) {
+        tasks.register<JavaExec>(file.name.removeSuffix(".kt")) {
+            group = "run"
+            description = "Runs ${file.name}"
+            classpath = sourceSets["main"].runtimeClasspath
+            mainClass = file.name.removeSuffix(".kt")
+        }
+    }
+}
