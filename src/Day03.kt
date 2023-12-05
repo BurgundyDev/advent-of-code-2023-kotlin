5625d2fa24252ae3baced15947cf60bc1998e408
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2023-12-05 09:17:16 +0000

    fix compilation error on Codespace

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,28 +2,28 @@ fun main() {
     fun getAdjacentCoords(coord: Pair<Int, Int>, schematic: List<List<Char>>): List<Pair<Int, Int>> {
         val list: MutableList<Pair<Int, Int>> = mutableListOf()
         if (coord.first > 0) {
-            list.addLast(Pair(coord.first - 1, coord.second))
+            list.add(Pair(coord.first - 1, coord.second))
         }
         if (coord.first < schematic.size - 1) {
-            list.addLast(Pair(coord.first + 1, coord.second))
+            list.add(Pair(coord.first + 1, coord.second))
         }
         if (coord.second > 0) {
-            list.addLast(Pair(coord.first, coord.second - 1))
+            list.add(Pair(coord.first, coord.second - 1))
         }
         if (coord.second < schematic[0].size - 1) {
-            list.addLast(Pair(coord.first, coord.second + 1))
+            list.add(Pair(coord.first, coord.second + 1))
         }
         if (coord.first > 0 && coord.second > 0) {
-            list.addLast(Pair(coord.first - 1, coord.second - 1))
+            list.add(Pair(coord.first - 1, coord.second - 1))
         }
         if (coord.first > 0 && coord.second < schematic[0].size - 1) {
-            list.addLast(Pair(coord.first - 1, coord.second + 1))
+            list.add(Pair(coord.first - 1, coord.second + 1))
         }
         if (coord.first < schematic.size - 1 && coord.second > 0) {
-            list.addLast(Pair(coord.first + 1, coord.second - 1))
+            list.add(Pair(coord.first + 1, coord.second - 1))
         }
         if (coord.first < schematic.size - 1 && coord.second < schematic[0].size - 1) {
-            list.addLast(Pair(coord.first + 1, coord.second + 1))
+            list.add(Pair(coord.first + 1, coord.second + 1))
         }
         return list
     }
@@ -68,12 +68,12 @@ fun main() {
                     if (currentNumber == 0)
                     {
                         currentNumber = cell.digitToInt()
-                        currentCoords.addLast(Pair(ri, ci))
+                        currentCoords.add(Pair(ri, ci))
                     }
                     else
                     {
                         currentNumber = currentNumber * 10 + cell.digitToInt()
-                        currentCoords.addLast(Pair(ri, ci))
+                        currentCoords.add(Pair(ri, ci))
                     }
                 }
                 if ((!cell.isDigit() && currentNumber != 0) || (ci == row.size - 1 && currentNumber != 0)) {
@@ -99,25 +99,25 @@ fun main() {
             var currentCoords: MutableList<Pair<Int, Int>> = mutableListOf()
             for ((ci, cell) in row.withIndex()) {
                 if (cell == '*') {
-                    gears.addLast(Pair(ri, ci))
+                    gears.add(Pair(ri, ci))
                 }
                 if (cell.isDigit())
                 {
                     if (currentNumber == 0)
                     {
                         currentNumber = cell.digitToInt()
-                        currentCoords.addLast(Pair(ri, ci))
+                        currentCoords.add(Pair(ri, ci))
                     }
                     else
                     {
                         currentNumber = currentNumber * 10 + cell.digitToInt()
-                        currentCoords.addLast(Pair(ri, ci))
+                        currentCoords.add(Pair(ri, ci))
                     }
                 }
                 if ((!cell.isDigit() && currentNumber != 0) || (ci == row.size - 1 && currentNumber != 0)) {
                     if (checkIfValid2(schematic, currentCoords)) {
                         // println(currentNumber.toString() + " ")
-                        parts.addLast(Pair(currentNumber, currentCoords))
+                        parts.add(Pair(currentNumber, currentCoords))
                     }
                     currentNumber = 0
                     currentCoords = mutableListOf()
@@ -134,7 +134,7 @@ fun main() {
                 if(!coordinatesBlocked.contains(coord)){
                     for (part in parts) {
                         if (part.second.contains(coord)) {
-                            adjacentParts.addLast(part.first)
+                            adjacentParts.add(part.first)
                             coordinatesBlocked.addAll(part.second)
                         }
                     }
