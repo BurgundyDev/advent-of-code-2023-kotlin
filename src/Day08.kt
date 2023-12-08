040c44b05d1854962f444f0235403c8e2262c311
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2023-12-08 15:40:43 +0100

    Day 8

---
+++ src/Day08.kt
@@ -0,0 +1,71 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val instructions: List<Char> = input[0].toList()
+        val map: MutableMap<String, Pair<String, String>> = mutableMapOf()
+        val regex = """(\w{3}) = \((\w{3}), (\w{3})\)""".toRegex()
+        for (line in input.drop(2)) {
+            regex.matchEntire(line)?.destructured?.let { (key, value1, value2) ->
+                map[key] = Pair(value1, value2)
+            }
+        }
+        var currentNode = "AAA"
+        val instructionIterator = CircularIterator(instructions)
+        var steps = 0
+        while(currentNode != "ZZZ") {
+            val currentInstruction = instructionIterator.next()
+            when (currentInstruction) {
+                'L' -> currentNode = map[currentNode]!!.first
+                'R' -> currentNode = map[currentNode]!!.second
+            }
+            steps++
+        }
+        return steps
+    }
+
+    fun part2(input: List<String>): Long {
+        val instructions: List<Char> = input[0].toList()
+        val map: MutableMap<String, Pair<String, String>> = mutableMapOf()
+        val regex = """(\w{3}) = \((\w{3}), (\w{3})\)""".toRegex()
+        for (line in input.drop(2)) {
+            regex.matchEntire(line)?.destructured?.let { (key, value1, value2) ->
+                map[key] = Pair(value1, value2)
+            }
+        }
+        val nodes = input.drop(2).map { it.split(" ")[0] }.filter { it.endsWith("A") }
+        val steps: MutableList<Long> = mutableListOf()
+        for (node in nodes) {
+            var currNode = node
+            var stepsForNode = 0L
+            val instructionIterator = CircularIterator(instructions)
+            while (!currNode.endsWith("Z")) {
+                val currentInstruction = instructionIterator.next()
+                when (currentInstruction) {
+                    'L' -> currNode = map[currNode]!!.first
+                    'R' -> currNode = map[currNode]!!.second
+                }
+                stepsForNode++
+            }
+            steps.add(stepsForNode)
+        }
+
+        return lcmOfList(steps)
+    }
+
+    // Test input solution
+    val testInput0 = readInput("Day08_test0")
+    val testInput1 = readInput("Day08_test1")
+    println("Expected Test for Part 1-0: 2")
+    part1(testInput0).println()
+    println("Expected Test for Part 1-1: 6")
+    part1(testInput1).println()
+    val testInput2 = readInput("Day08_test2")
+    println("Expected Test for Part 2-2: 6")
+    part2(testInput2).println()
+
+    // Real input solution
+    val input = readInput("Day08")
+    println("Part 1: ")
+    part1(input).println()
+    println("Part 2: ")
+    part2(input).println()
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,20 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+class CircularIterator<T>(private val list: List<T>) : Iterator<T> {
+    private var currentIndex = -1
+
+    override fun hasNext(): Boolean {
+        return list.isNotEmpty()
+    }
+
+    override fun next(): T {
+        currentIndex = (currentIndex + 1) % list.size
+        return list[currentIndex]
+    }
+}
+
+fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+fun lcm(a: Long, b: Long): Long = (a * b) / gcd(a, b)
+fun lcmOfList(numbers: List<Long>): Long = numbers.reduce(::lcm)
